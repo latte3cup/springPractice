0fa08913739f2b890f88cc0bea587485d5843f71
latte3cup <latte3cup@naver.com>
2024-09-03 16:20:30 +0900

    ch9 까지 완료

---
+++ sq-ch9-ex2/src/test/java/main/sq_ch9_ex1/SqCh9Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.sq_ch9_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh9Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
